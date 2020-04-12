cf84f441ce70ecc56e5e0b34b96a5376873a1b46
tausvels <engr.tausifkhan@gmail.com>
2020-04-12 15:40:48 -0700

    added composeEnahncer to prepare redux to accept middlewares

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, compose} from 'redux';
 // ---- import all the reducers in this file ------ //
 import placesReducer from './reducers/places';
 
@@ -6,8 +6,16 @@ const rootReducer = combineReducers({
   places: placesReducer,
 });
 
+// ---- All middlewares are enhancers ----------------- //
+// ---- window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ is an enhancer ------- //
+let composeEnhancers = compose;
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  // for multiple middleware, put those as args for the composeEnhancers
+  return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;
